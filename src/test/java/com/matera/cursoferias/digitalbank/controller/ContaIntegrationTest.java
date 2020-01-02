b3cf5638eaa08eb11ad96711ea761160ba89a056
zessin <gabrielzessin@hotmail.com>
2020-01-02 15:35:05 -0200

    Renomeando classe de teste de integração

--- src/test/java/com/matera/cursoferias/digitalbank/controller/ContaIntegrationTest.java
+++ src/test/java/com/matera/cursoferias/digitalbank/controller/ContaIntegrationTest.java
@@ -33,29 +33,27 @@ import io.restassured.specification.RequestSpecification;
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
-public class ContaIntegracaoTest {
+public class ContaIntegrationTest {
 
 	private static final String URL_BASE = "digitalbank/api/v1/contas";
-	
+
 	private ResponseDTO<ContaResponseDTO> contaResponse;
-	
+
 	@BeforeEach
 	public void buildCliente() {
 		ClienteRequestDTO clienteRequest = buildClienteRequestDTO();
 
-		ResponseDTO<ContaResponseDTO> response = buildPostRequest(clienteRequest, "digitalbank/api/v1/clientes", HttpStatus.CREATED).
+		contaResponse = buildPostRequest(clienteRequest, "digitalbank/api/v1/clientes", HttpStatus.CREATED).
 			extract().
 				body().
 					as(new TypeRef<ResponseDTO<ContaResponseDTO>>() {});
-		
-		this.contaResponse = response;
 	}
-	
+
 	@Test
-	public void efetuarDepositoTest() {
+	public void efetuaDepositoTest() {
 		BigDecimal valor = new BigDecimal(100);
 		String descricao = "Depósito";
-		
+
 		efetuaDeposito(valor, descricao).
 			root("dados").
 				body("idLancamento", greaterThan(0)).
@@ -65,7 +63,7 @@ public class ContaIntegracaoTest {
 				body("natureza", equalTo(Natureza.CREDITO.getCodigo())).
 				body("tipoLancamento", equalTo(TipoLancamento.DEPOSITO.getCodigo())).
 				body("descricao", equalTo(descricao));
-		
+
 		RequestSpecification requestSpecification = new RequestSpecBuilder().
 			addPathParam("id", contaResponse.getDados().getIdCliente()).
 			build();
@@ -76,13 +74,13 @@ public class ContaIntegracaoTest {
 	}
 
 	@Test
-	public void efetuarSaqueTest() {
+	public void efetuaSaqueTest() {
 		efetuaDeposito(new BigDecimal(100), "Depósito");
-		
+
 		LancamentoRequestDTO lancamento = new LancamentoRequestDTO();
 		lancamento.setValor(new BigDecimal(50));
 		lancamento.setDescricao("Saque");
-		
+
 		buildPostRequest(lancamento, URL_BASE + "/" + contaResponse.getDados().getIdConta() + "/sacar", HttpStatus.OK).
 			root("dados").
 				body("idLancamento", greaterThan(0)).
@@ -92,7 +90,7 @@ public class ContaIntegracaoTest {
 				body("natureza", equalTo(Natureza.DEBITO.getCodigo())).
 				body("tipoLancamento", equalTo(TipoLancamento.SAQUE.getCodigo())).
 				body("descricao", equalTo(lancamento.getDescricao()));
-		
+
 		RequestSpecification requestSpecification = new RequestSpecBuilder().
 			addPathParam("id", contaResponse.getDados().getIdCliente()).
 			build();
@@ -101,15 +99,15 @@ public class ContaIntegracaoTest {
 			root("dados").
 				body("saldo", equalTo(lancamento.getValor().floatValue()));
 	}
-	
+
 	@Test
-	public void efetuarPagamentoTest() {
+	public void efetuaPagamentoTest() {
 		efetuaDeposito(new BigDecimal(100), "Depósito");
-		
+
 		LancamentoRequestDTO lancamento = new LancamentoRequestDTO();
 		lancamento.setValor(new BigDecimal(50));
 		lancamento.setDescricao("Pagamento");
-		
+
 		buildPostRequest(lancamento, URL_BASE + "/" + contaResponse.getDados().getIdConta() + "/pagar", HttpStatus.OK).
 			root("dados").
 				body("idLancamento", greaterThan(0)).
@@ -119,7 +117,7 @@ public class ContaIntegracaoTest {
 				body("natureza", equalTo(Natureza.DEBITO.getCodigo())).
 				body("tipoLancamento", equalTo(TipoLancamento.PAGAMENTO.getCodigo())).
 				body("descricao", equalTo(lancamento.getDescricao()));
-		
+
 		RequestSpecification requestSpecification = new RequestSpecBuilder().
 			addPathParam("id", contaResponse.getDados().getIdCliente()).
 			build();
@@ -128,12 +126,12 @@ public class ContaIntegracaoTest {
 			root("dados").
 				body("saldo", equalTo(lancamento.getValor().floatValue()));
 	}
-	
+
 	private ValidatableResponse efetuaDeposito(BigDecimal valor, String descricao) {
 		LancamentoRequestDTO lancamento = new LancamentoRequestDTO();
 		lancamento.setValor(valor);
 		lancamento.setDescricao(descricao);
-		
+
 		return buildPostRequest(lancamento, URL_BASE + "/" + contaResponse.getDados().getIdConta() + "/depositar", HttpStatus.OK);
 	}
 }
